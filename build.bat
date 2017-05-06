b0ea4ac6575d4c64b61354e0f44f3414a1d2d5a1
Solomon QWE <solomon-qwe@users.noreply.github.com>
2017-05-06 14:36:39 +0900

    Delete unnecessary configuration, and use Visual Studio Installer.

--- build.bat
+++ build.bat
@@ -3,10 +3,6 @@ if not exist log mkdir log
 
 set devenv="%VS140COMNTOOLS%..\IDE\devenv.com"
 
-%devenv% agalia.sln /Rebuild "Release_static|x64" /Out log\build_x64_Release_static.log
-%devenv% agalia.sln /Rebuild "Release_static|x86" /Out log\build_x86_Release_static.log
-%devenv% agalia.sln /Rebuild "Debug_static|x64" /Out log\build_x64_Debug_static.log
-%devenv% agalia.sln /Rebuild "Debug_static|x86" /Out log\build_x86_Debug_static.log
 %devenv% agalia.sln /Rebuild "Release|x64" /Out log\build_x64_Release.log
 %devenv% agalia.sln /Rebuild "Release|x86" /Out log\build_x86_Release.log
 %devenv% agalia.sln /Rebuild "Debug|x64" /Out log\build_x64_Debug.log
--- vstest.bat
+++ vstest.bat
@@ -3,10 +3,6 @@ if not exist log mkdir log
 
 set testproc="%VS140COMNTOOLS%..\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe"
 
-%testproc% x64\Release_static\unit_test.dll /Diag:log\ut_x64rel_sta.log /Platform:x64
-%testproc% x86\Release_static\unit_test.dll /Diag:log\ut_x86rel_sta.log /Platform:x86
-%testproc% x64\Debug_static\unit_test.dll /Diag:log\ut_x64dbg_sta.log /Platform:x64
-%testproc% x86\Debug_static\unit_test.dll /Diag:log\ut_x86dbg_sta.log /Platform:x86
 %testproc% x64\Release\unit_test.dll /Diag:log\ut_x64rel_dyn.log /Platform:x64
 %testproc% x86\Release\unit_test.dll /Diag:log\ut_x86rel_dyn.log /Platform:x86
 %testproc% x64\Debug\unit_test.dll /Diag:log\ut_x64dbg_dyn.log /Platform:x64
