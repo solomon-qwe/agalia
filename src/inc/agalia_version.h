906017bdb490a9602d03e4fdba03dbb9c56e8dfb
Solomon QWE <solomon-qwe@users.noreply.github.com>
2025-03-16 21:21:45 +0900

    Update version information for v1.5.1

--- src/inc/agalia_version.h
+++ src/inc/agalia_version.h
@@ -1,10 +1,10 @@
 #pragma once
 
 // major, minor, revision, reserved 
-#define AGALIA_VERSION			1,5,0,0
-#define AGALIA_VERSION_STRING	"1.5.0.0"
-#define AGALIA_ABOUT_STRING		"agalia, Version 1.5.0"
+#define AGALIA_VERSION			1,5,1,0
+#define AGALIA_VERSION_STRING	"1.5.1.0"
+#define AGALIA_ABOUT_STRING		"agalia, Version 1.5.1"
 #define AGALIA_COPYRIGHT_STRING	"Copyright (C) 2017-2025 Solomon QWE"
 
 #define AGALIA_MANUFACTURER	"Solomon QWE"
-#define AGALIA_APPID		"SolomonQWE.Agalia.1.5.0"
+#define AGALIA_APPID		"SolomonQWE.Agalia.1.5.1"
